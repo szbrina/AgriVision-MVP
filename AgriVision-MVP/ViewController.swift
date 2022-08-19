91256bbe5941a1e699d5fa6699d8c18461f307cd
Sabrina Zhang <szcamino@gmail.com>
2022-08-19 10:25:39 -0700

    Initial Commit

---
+++ AgriVision-MVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AgriVision-MVP
+//
+//  Created by Sabrina on 8/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
